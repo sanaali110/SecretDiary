610c915f584a384c3b373ecbc82c2b367fd40d68
sana <sana_tsm@yahoo.com>
2020-07-04 16:13:09 +0100

    Update index.php

--- index.php
+++ index.php
@@ -131,11 +131,7 @@ if(mysqli_num_rows($result)> 0 ){
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
   <script type="text/javascript">
   $(document).ready(function() {
-/*
-    $("form").submit(function(e){
-        e.preventDefault();
-    });
-*/
+
   });
    
   </script>
